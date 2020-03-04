687b8e103b019e2018cdb429fed6212f1677566b
Euler Faria <Euler.Faria@vale.com>
2020-03-04 13:13:48 -0300

    Pipeline with Scaler, PCA, Model

--- my_app.py
+++ my_app.py
@@ -1,21 +1,14 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from data_preparation import get_data, split
-from train import hpo
+from train import hpo, build_pipeline
 import sklearn
 
+
 @hydra.main(config_path="conf/config.yaml")
-def my_app(cfg: DictConfig):
-    primitive = OmegaConf.to_container(cfg, resolve=True)
-    print(primitive['dataset'])
-    return primitive
-
-
-if __name__ == "__main__":
-    cfg = my_app()
-    print(cfg)
-    base_conf = OmegaConf.load('conf/config.yaml')
-    print(base_conf.pretty())
+def my_app(cfg: DictConfig)-> None:
+    cfg = OmegaConf.to_container(cfg, resolve=True)
+    
 
     X, y = get_data()
     X_train, X_test, y_train, y_test = split(X, y, 0.2)
@@ -23,9 +16,14 @@ if __name__ == "__main__":
     print(X_test.shape)
 
     
-    #print(cfg['gs_params'])
-
-    model = hpo(X_train, y_train, cfg)
+    pipe = build_pipeline(cfg)
+    model = hpo(pipe,X_train, y_train,cfg)
     
     print(model)
+
+
+if __name__ == "__main__":
+    my_app()
+    
+
     
--- train.py
+++ train.py
@@ -5,19 +5,47 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
 
 
-def hpo(X_train, y_train, cfg):
-    pl = Pipeline([
-        ('scalar', StandardScaler()),
-        ('pca', PCA()),
-        ('lr', LogisticRegression())
-    ])
+def build_pipeline(cfg):
 
+    pipe = []
     
-    print(cfg['gs_params'])
-    gs = GridSearchCV(pl, cfg['gs_params'], cv=5, scoring='f1')
+    if cfg['scaling']['type'] == 'std':
+        pipe.append(
+            ('sc',StandardScaler())
+            ) 
+    elif cfg['scaling']['type'] == 'minmax':      
+        pipe.append(
+            ('sc',MinMaxScaler())
+            ) 
+
+    if cfg['decomp']['type'] == 'pca':
+        pipe.append(
+            ('pca',PCA())
+            ) 
+
+    if cfg['model']['type'] =='lr':
+        pipe.append(
+            ('lr',LogisticRegression())
+        )
+
+    return Pipeline(pipe)
+
+def hpo(pipe,X_train, y_train, cfg):
+    
+    cfg['model'].pop('type', None)
+
+    cfg['decomp'].pop('type', None)
+    print(cfg)
+
+    args = {}
+    args.update(cfg['model'])
+    args.update(cfg['decomp'])
+
+
+    gs = GridSearchCV(pipe, args, cv=5, scoring='f1')
     gs.fit(X_train, y_train)
 
     return gs.best_estimator_
