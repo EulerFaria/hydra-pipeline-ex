7d4a90b9e028bf1273c4ff891db925efe6af1247
EulerFaria <eulerodriguesousa@gmail.com>
2020-07-22 20:57:42 -0300

    fixing import name functions, cleaning unused path name

--- train.py
+++ train.py
@@ -1,18 +1,15 @@
-from src.data.make_dataset import run as data_acq
-from src.features.build_features import run as data_prep
+from src.data.make_dataset import data_acq
+from src.features.build_features import  data_prep
 from src.models.train_model import train, split_data
 from src.report import generate_report
 from pathlib import Path
-from datetime import datetime as dt
 import hydra
-import os
 
 @hydra.main(config_path="config/config.yaml", strict=False)
 def run(cfg=None):
-    os.environ['path'] =  str(Path(__file__).resolve().parent)
-    # Getting dataset 
+    #Getting dataset 
     raw_data = data_acq(cfg)
-    # Building features
+    #Building features
     data = data_prep(raw_data, cfg)
     #Split data 
     X_train, X_test, y_train, y_test= split_data(data)
